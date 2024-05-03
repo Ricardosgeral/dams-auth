1de44cd352c3b8dad0fdc494ccb11a2b9e95526d
Ricardosgeral <ricardos.geral@gmail.com>
2024-05-03 16:06:36 +0100

    Admin things

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -7,11 +7,11 @@ interface ProtectLayoutProps {
 export default function ProtectedLayout({ children }: ProtectLayoutProps) {
   return (
     <div
-      className="w-full h-screen flex flex-col gap-y-10 items-center justify-center 
+      className="w-full min-h-screen flex flex-col py-3 gap-y-10 items-center justify-center
     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"
     >
       <NavBar></NavBar>
-      {children}
+      <div className="flex-grow">{children}</div>
     </div>
   );
 }
